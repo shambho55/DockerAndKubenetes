0f181a521934a6d38878ae44fa8fbb89bc5b2f23
shambho55 <rishabhchaturvedi579@gmail.com>
2026-02-02 01:15:50 +0530

    troubleshooting rollout failures and implemented hostname in v1.1.0
    Signed-off-by: shambho55 <rishabhchaturvedi579@gmail.com>

--- color-api/src/index.js
+++ color-api/src/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const os = require('os');
 
 const app = express();
 const port = 80;
+const color = 'blue';
+const hostname = os.hostname();
 
 app.get('/',(req,res) => {
-    res.send('<h1 style="color:blue;">Hello from color-api!</h1>');
+    res.send(`<h1 style="color:${color};">Hello from color-api!</h1>
+<h2>Hostname:${hostname}</h2>`);
+});
+
+app.get('/api',(req,res) => {
+    const {format} = req.query // localhost/api?format=text
+
+    if(format == 'json'){
+    res.json({
+        color,
+        hostname
+    });
+    }
+    else{
+        return res.send(`COLOR : ${color}, HOSTNAME : ${hostname}`);
+    }
+
 });
 
 app.listen(port, () => {
